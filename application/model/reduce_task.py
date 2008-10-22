4df9049ed485bede0f4629480bcc7c571dd05f39
Mark Chadwick <mark.chadwick@gmail.com>
2008-10-21 22:24:12 -0400

    Moving remaining app files to application

---
+++ application/model/reduce_task.py
@@ -0,0 +1,8 @@
+from google.appengine.ext import db
+
+from app.models.job import Job
+from app.models.task import Task
+
+class ReduceTask(Task):
+    job = db.ReferenceProperty(Job, collection_name='reduce_tasks')
+    
